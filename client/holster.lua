a7a564206c644497470e19ffd2ce44549fdcc003
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-03-26 21:42:27 +0100

    feat: holster components

---
+++ client/holster.lua
@@ -0,0 +1,44 @@
+local DATA_HOLSTERS <const> = lib.load("data.holsters")
+
+local function getGender(ped)
+    local model = GetEntityModel(ped)
+    if model == `mp_m_freemode_01` then
+        return "male"
+    elseif model == `mp_f_freemode_01` then
+        return "female"
+    end
+end
+
+local function holsterCheck(ped, playerWeapon, playerHolster, shouldUnholster)
+    local gender = getGender(ped)
+    if not gender then return end
+
+    for i=1, #DATA_HOLSTERS do
+        local holster = DATA_HOLSTERS[i]
+        local holsterInfo = holster[gender]
+
+        if not holsterInfo or not lib.table.contains(holster.weapons, playerWeapon) then goto next end
+
+        if playerHolster == holsterInfo[1] and shouldUnholster then
+            Wait(200)
+            SetPedComponentVariation(ped, 7, holsterInfo[2], 0, 0)
+            return --print("Gun unholstered")
+        elseif playerHolster == holsterInfo[2] then
+            SetPedComponentVariation(ped, 7, holsterInfo[1], 0, 0)
+            return --print("Gun holstered")
+        end
+
+        ::next::
+    end
+end
+
+lib.onCache("weapon", function(value)
+    local ped = cache.ped
+    local playerHolster = GetPedDrawableVariation(ped, 7)
+
+    if not value then
+        return holsterCheck(ped, cache.weapon, playerHolster, false)
+    end
+
+    holsterCheck(ped, value, playerHolster, true)
+end)
---
+++ data/holster.lua
@@ -0,0 +1,16 @@
+-- change holster clothing when pulling out or hosltering weapon. Male & female keys have these: {holstered, unholstered}
+return {
+    {
+        weapons = {`weapon_combatpistol`},
+        male = {182, 186}
+    },
+    {
+        weapons = {`weapon_combatpistol`},
+        male = {179, 183},
+        female = {148, 149}
+    },
+    {
+        weapons = {`weapon_combatpistol`},
+        male = {187, 188}
+    }
+}
