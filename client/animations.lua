afbf7bc8416f6da3aba8e8a403e227c5902b18b4
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-03-28 23:22:16 +0100

    feat: weapon holster/unholster anims

---
+++ client/animations.lua
@@ -0,0 +1,202 @@
+local DATA_ANIMATIONS <const> = lib.load("data.animations")
+local USING_INVENTORY <const> = GetResourceState("ox_inventory"):find("start") and GetConvarInt("inventory:weaponanims", 1) ~= 1
+local complete = true
+
+local function isVehicleBlocked(vehicle)
+    if not vehicle then return end
+
+    local class = GetVehicleClass(vehicle)
+    return class == 8 or class == 13
+end
+
+local function isBlocked(ped)
+    return isVehicleBlocked(cache.vehicle) or GetPedParachuteState(ped) > 0
+end
+
+local function getGender(ped)
+    local model = GetEntityModel(ped)
+    if model == `mp_m_freemode_01` then
+        return "male"
+    elseif model == `mp_f_freemode_01` then
+        return "female"
+    end
+end
+
+local function getWeaponAnim(ped, weapon, variant)
+    local animations = DATA_ANIMATIONS.animations
+    local weaponGroups = DATA_ANIMATIONS?.weaponGroups
+    local weaponAnims = DATA_ANIMATIONS?.weapons
+    local clothingInfo = DATA_ANIMATIONS?.clothing?[variant]
+    local anim = nil
+    local currentWeaponGroup = GetWeapontypeGroup(weapon)
+
+    local animByGroup = weaponGroups?[currentWeaponGroup]
+    if animByGroup or animByGroup == false then
+        anim = animByGroup
+    end
+
+    local animByWeapon = weaponAnims?[weapon]
+    if animByWeapon or animByWeapon == false then
+        anim = animByWeapon
+    end
+
+    if clothingInfo and lib.table.contains(clothingInfo.weapons, weapon) then
+        local gender = getGender(ped)
+        local components = clothingInfo[gender]
+        if not components then goto skip end
+
+        local holster = GetPedDrawableVariation(ped, clothingInfo.variation)
+        if not lib.table.contains(components, holster) then goto skip end
+
+        anim = clothingInfo.anim
+    end
+
+    ::skip::
+
+    if anim == nil then
+        anim = DATA_ANIMATIONS.default
+    end
+
+    return animations[anim]
+end
+
+local function startWaponAnim(ped, info, weapon, hideWeapon)
+    local coords = GetEntityCoords(ped)
+    lib.requestAnimDict(info.dict)
+    TaskPlayAnimAdvanced(ped, info.dict, info.clip, coords.x, coords.y, coords.z, 0, 0, GetEntityHeading(ped), 8.0, 3.0, info.duration*2, 50, 0.1)
+    RemoveAnimDict(info.dict)
+
+    SetTimeout(info.clothing or info.duration/2, function()
+        TriggerEvent("ND_GunAnims:updateHolster", weapon, hideWeapon)
+    end)
+
+    Wait(info.duration)
+    
+    if info.cancel then
+        StopAnimTask(ped, info.dict, info.clip, 2.0)
+    end
+end
+
+local function playWeaponAnim(weapon, variant)
+    local ped = cache.ped
+    local info = getWeaponAnim(ped, weapon, variant)
+
+    if not info or isBlocked(ped) then
+        return
+    end
+
+    info = info[variant]
+    if not info then return end
+
+    if info.dict then
+        startWaponAnim(ped, info, weapon, variant == "unholster")
+    else
+        local anims = info
+        for i=1, #anims do
+            local info = anims[i]
+            startWaponAnim(ped, info, weapon, variant == "unholster")
+        end
+    end
+end
+
+function thread(variant, ped, hash)
+    LocalPlayer.state.invBusy = true
+
+    if variant == "holster" then
+        CreateThread(function()
+            SetPedCurrentWeaponVisible(ped, false, false)
+            DisablePlayerFiring(cache.playerId, true)
+
+            while not complete do
+                DisableControlAction(1, 25, true)
+                DisableControlAction(1, 68, true)
+                DisableControlAction(1, 70, true)
+                DisableControlAction(1, 91, true)
+                Wait(0)
+            end
+
+            DisablePlayerFiring(cache.playerId, false)
+            SetPedCurrentWeaponVisible(ped, true, false)
+        end)
+    else
+        CreateThread(function()
+            DisablePlayerFiring(cache.playerId, true)
+            while not complete do
+                if USING_INVENTORY then                 
+                    GiveWeaponToPed(ped, hash, 0, false, true)
+                    SetCurrentPedWeapon(ped, hash, true)
+                end
+                DisableControlAction(1, 25, true)
+                DisableControlAction(1, 68, true)
+                DisableControlAction(1, 70, true)
+                DisableControlAction(1, 91, true)
+                Wait(0)
+            end
+            DisablePlayerFiring(cache.playerId, false)
+        end)
+    end
+
+    LocalPlayer.state.invBusy = false
+end
+
+if USING_INVENTORY then
+    local items = exports.ox_inventory:Items()
+
+    AddEventHandler("ox_inventory:usedItem", function(name, slotId, metadata)
+        local data = items[name]
+        if not data or not data.weapon or data.ammo then return end
+
+        complete = false
+        local ped = cache.ped
+        
+        thread("holster", ped, data.hash)
+        playWeaponAnim(data.hash, "unholster")
+        complete = true
+    end)
+
+    AddEventHandler("ox_inventory:currentWeapon", function(weapon)
+        local hash = cache.weapon
+        local ped = cache.ped
+        if not hash or weapon then return end
+
+        complete = false
+
+        thread("unholster", ped, hash)
+        playWeaponAnim(hash, "holster")
+        complete = true
+    end)
+end
+
+if not USING_INVENTORY then
+    local _, weapon = GetCurrentPedWeapon(cache.ped, true)
+    local weaponInHand = nil
+
+    local function weaponChanged(value)
+        local ped = cache.ped
+        local hash = value or weapon
+        complete = false
+
+        if value then
+            thread("holster", ped, hash)
+            playWeaponAnim(hash, "unholster")
+        else
+            thread("unholster", ped, hash)
+            playWeaponAnim(hash, "holster")
+        end
+        
+        complete = true
+    end
+
+    CreateThread(function()
+        while true do
+            local _, currentWeapon = GetCurrentPedWeapon(cache.ped, true)
+            if currentWeapon ~= weapon then
+                local new = currentWeapon ~= `WEAPON_UNARMED` and currentWeapon
+                weaponChanged(new)
+                weaponInHand = new
+                weapon = currentWeapon
+            end
+            Wait(0)
+        end
+    end)
+end
---
+++ data/animations.lua
@@ -0,0 +1,52 @@
+-- animatios when weapon is unholstered and holstered.
+local info = {}
+
+info.animations = {
+    mele = {
+        holster = { dict = "melee@holster", clip = "holster", duration = 200 },
+        unholster = { dict = "melee@holster", clip = "unholster", duration = 200 }
+    },
+    gang = {
+        holster = { dict = "reaction@intimidation@1h", clip = "outro", duration = 800 },
+        unholster = { dict = "reaction@intimidation@1h", clip = "intro", duration = 900 }
+    },
+    police = {
+        holster = { dict = "reaction@intimidation@cop@unarmed", clip = "intro", duration = 400, clothing = 300 },
+        unholster = { dict = "rcmjosh4", clip = "josh_leadout_cop2", duration = 300, clothing = 0, cancel = true }
+    }
+}
+
+-- default anim for weapons not found in groups, weapons, or clothing.
+info.default = "gang"
+
+-- weapon groups can be found here: https://docs.fivem.net/natives/?_0xC3287EE3050FB74C
+info.weaponGroups = {
+    [`GROUP_MELEE`] = "mele",
+    [`GROUP_PISTOL`] = "gang",
+}
+
+-- If you add a weapon here it will ignore the one on it's group and instead play a specific anim set here.
+info.weapons = {
+    [`WEAPON_STUNGUN`] = "police",
+    [`WEAPON_SWITCHBLADE`] = false -- setting to false will not play any anim and instead use the in game default.
+}
+
+-- if wearing specific clothing such as a holster then everything else ignored and that anim is used.
+info.clothing = {
+    holster = {
+        anim = "police",
+        weapons = {`WEAPON_COMBATPISTOL`},
+        variation = 7,
+        male = {186, 183, 188},
+        female = {149}
+    },
+    unholster = {
+        anim = "police",
+        weapons = {`WEAPON_COMBATPISTOL`},
+        variation = 7,
+        male = {182, 179, 187},
+        female = {148}
+    }
+}
+
+return info
